00d36dcbeb0b79e1511f482f525e9f666d86987c
Evgenii Kazakov <evgenii.kazakov@aimtech.com>
2018-12-12 22:42:45 +0300

    common: add secondary x_transformer

---
+++ kaggle/the_new_order/x_transformer/regboost_x_transformer.py
@@ -0,0 +1,47 @@
+import sys
+sys.path.append("../../base")
+from common import *
+
+
+
+
+
+
+
+#----------regboost_x_transformer----------
+
+class RegboostXTransformer:
+    def __init__(self, config):
+        self.log = logging.getLogger("RegboostXTransformer")
+        self.log.info("x_transformer config: {0}".format(config))
+        self.config = config
+        self.model = cb.CatBoostRegressor(
+                #logging_level="Silent",
+                loss_function=config["loss_function"],
+                #classes_count=self.config["classes_count"],
+                iterations=config["iterations"],
+                l2_leaf_reg=config["l2_leaf_reg"],
+                learning_rate=config["learning_rate"],
+                #bagging_temperature=self.config["bagging_temperature"],
+                depth=config["depth"],
+                thread_count=19,
+                metric_period=10,
+                random_state=42
+                #one_hot_max_size=self.config["one_hot_max_size"]
+        )
+        self.log.info("inited")
+
+    def load_train_data(self, x_train, y_train):
+        self.log.info("load x_train size: {0} y_train size: {1}".format(len(x_train), len(y_train)))
+        self.model.fit(x_train, y_train)
+        self.log.info("loaded")
+
+    def transform(self, x_data):
+        self.log.info("transform x_data size: {0}".format(len(x_data)))
+        prediction = self.model.predict(x_data).reshape(-1, 1)
+        x_data = np.concatenate((x_data, prediction), axis=1)
+        x_data = np.concatenate((x_data, np.log(prediction)), axis=1)
+        #result = np.concatenate((result, np.sqrt(prediction)), axis=1)
+        #result = np.concatenate((result, prediction ** 2), axis=1)
+        self.log.info("transformed")
+        return x_data
